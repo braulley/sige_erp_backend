41642084b9a31040d6e73f4be16a6d1f6bf70a36
braulley <juniorbraulley@gmail.com>
2018-05-08 01:41:57 -0300

    fix token

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,8 +46,19 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+    /*public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
+    }*/
+
+    public function render($request, Exception $exception)
+    {
+        if ($exception instanceof Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+            return response()->json(['token_expired'], $exception->getStatusCode());
+        } else if ($exception instanceof Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+            return response()->json(['token_invalid'], $exception->getStatusCode());
+        }
+
+	    return parent::render($request, $exception);
     }
 }
