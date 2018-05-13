d2a4363035c3d14b7f82588a30abd6d8feed5ddb
braulley <juniorbraulley@gmail.com>
2018-05-13 15:08:05 -0300

    herokus

--- config/logging.php
+++ config/logging.php
@@ -36,9 +36,12 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            
+            'driver' => 'errorlog',
             'level' => 'debug',
+            /*'driver' => 'single',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => 'debug',*/
         ],
 
         'daily' => [
